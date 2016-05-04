b194fbde307d72a641173f62c0ee9dfd26e00dca
LakshmiEvani <Souji@Ravi-Evanis-MacBook-Pro.local>
2016-05-04 18:24:07 -0400

    Customized toolbar items like pick, delete

--- Meme Version 2.0/MemeDetailViewController.swift
+++ Meme Version 2.0/MemeDetailViewController.swift
@@ -20,11 +20,13 @@ class MemeDetailViewController:UIViewController {
         super.viewWillAppear(animated)
         detailImageView.image = meme.memedImage;
         tabBarController?.tabBar.hidden = true
+        navigationController?.setToolbarHidden(true, animated: true)
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         tabBarController?.tabBar.hidden = false
+         navigationController?.setToolbarHidden(false, animated: false)
     }
     
     @IBAction func EditMeme(sender: AnyObject) {
