d150264a9d641faf7e4d18e943ea8de4dcfde494
LakshmiEvani <Souji@Ravi-Evanis-MacBook-Pro.local>
2016-05-06 10:50:07 -0400

    Assigned delegate and datasource to the table view

--- Meme Version 2.0/MemeTableViewController.swift
+++ Meme Version 2.0/MemeTableViewController.swift
@@ -28,8 +28,10 @@ class MemeTableViewController: UIViewController, UITableViewDataSource, UITableV
     override func viewWillAppear(animated: Bool) {
         
         super.viewWillAppear(animated)
-        
+        self.meMeTableView?.delegate = self
+        self.meMeTableView?.dataSource = self
         meMeTableView?.reloadData()
+        
     }
     
     
